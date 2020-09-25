85957947408f0afe3be605ff38540e8c11ff8339
ravindraindia4u <ravindraindia4u@gmail.com>
2020-09-26 03:12:48 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -1,117 +1,114 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const port = 3000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // your code goes here
-app.get("/",(req, res) => {
-    res.send("Hello world!");
+app.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 
 // addition operation
-app.post("/add",(req, res) => {
-    const num = 1000000;
-    let num1 = req.body.num1;
-    let num2 = req.body.num2;
-    let sum = num1 + num2;
-    if(typeof num1 === "string" || typeof num2 === "string"){
-        res.json({
-            status:"error",
-            message: "Invalid data types"
-        });
-    }else if(num1 >= num || num2 >= num || sum >= num){
-        res.json({
-            status: "failure",
-            message: "Overflow"
-        });
-    }else{
-        res.json({
-            status: "success",
-            message: "the sum of given two numbers",
-            sum: sum
-        });
-    }
+app.post("/add", (req, res) => {
+  const num = 1000000;
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  let sum = num1 + num2;
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return res.json({
+      status: "error",
+      message: "Invalid data types",
+    });
+  } else if (num1 >= num || num2 >= num || sum >= num) {
+    return res.json({
+      status: "failure",
+      message: "Overflow",
+    });
+  } else {
+    return res.json({
+      status: "success",
+      message: "the sum of given two numbers",
+      sum: sum,
+    });
+  }
 });
 
 // Subtraction Operation
-app.post("/sub",(req, res) => {
-    const num = -1000000;
-    let num1 = req.body.num1;
-    let num2 = req.body.num2;
-    
-    if(typeof num1 === "string" || typeof num2 === "string"){
-        res.json({
-            status:"error",
-            message: "Invalid data types"
-        });
-    }else if(num1 <= num || num2 <= num ){
-        res.json({
-            status: "failure",
-            message: "Underflow"
-        });
-    }else{
-        let sub = num1 - num2;
-        res.json({
-            status: "success",
-            message: "the difference of given two numbers",
-            difference: sub
-        });
-    }
+app.post("/sub", (req, res) => {
+  const num = -1000000;
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return res.json({
+      status: "error",
+      message: "Invalid data types",
+    });
+  } else if (num1 <= num || num2 <= num) {
+    return res.json({
+      status: "failure",
+      message: "Underflow",
+    });
+  } else {
+    let sub = num1 - num2;
+    return res.json({
+      status: "success",
+      message: "the difference of given two numbers",
+      difference: sub,
+    });
+  }
 });
 
 // Multiplication Operation
-app.post("/multiply",(req, res) => {
-    const num = 1000000;
-    let num1 = req.body.num1;
-    let num2 = req.body.num2;
-    let mul = num1 * num2;
-    if(typeof num1 === "string" || typeof num2 === "string"){
-        res.json({
-            status:"error",
-            message: "Invalid data types"
-        });
-    }else if(num1 >= num || num2 >= num || mul >= num){
-        res.json({
-            status: "failure",
-            message: "Overflow"
-        });
-    }else{
-        res.json({
-            status: "success",
-            message: "The product of given numbers",
-            result: mul
-        });
-    }
+app.post("/multiply", (req, res) => {
+  const num = 1000000;
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  let mul = num1 * num2;
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return res.json({
+      status: "error",
+      message: "Invalid data types",
+    });
+  } else if (num1 >= num || num2 >= num || mul >= num) {
+    return res.json({
+      status: "failure",
+      message: "Overflow",
+    });
+  } else {
+    return res.json({
+      status: "success",
+      message: "The product of given numbers",
+      result: mul,
+    });
+  }
 });
 
 // Division Operation
 app.post("/divide", (req, res) => {
-    let num1 = req.body.num1;
-    let num2 = req.body.num2;
-    if (num2 === 0) {
-      res.json({ 
-          message: "Cannot divide by zero" 
-        });
-    }
-    let div = num1 / num2;
-    res.json({
-        status:"success",
-        message: "The division of given numbers",
-        result: div
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  if (num2 === 0) {
+    return res.json({
+      message: "Cannot divide by zero",
     });
+  }
+  let div = num1 / num2;
+  res.json({
+    status: "success",
+    message: "The division of given numbers",
+    result: div,
   });
- 
+});
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
